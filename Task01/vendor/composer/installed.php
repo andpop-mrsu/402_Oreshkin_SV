b217cc33feefd4380e722835bf628e6d44ce04c5
Andrey Popov <andrvpopov@gmail.com>
2022-02-25 21:51:31 +0300

    Add task04

---
+++ Task01/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '67801658f8697f635f18aef156b09723d94fa420',
+    'name' => 'sergeyoreshkin/task01',
+  ),
+  'versions' => 
+  array (
+    'sergeyoreshkin/task01' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67801658f8697f635f18aef156b09723d94fa420',
+    ),
+  ),
+);
